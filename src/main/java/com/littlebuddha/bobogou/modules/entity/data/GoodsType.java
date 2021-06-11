287306e9ac0f0592b5335a743ea3c2fe72f2d61c
LittleBuddha-k <2998986128@qq.com>
2021-06-11 10:30:43 +0800

    小改

--- src/main/java/com/littlebuddha/bobogou/modules/entity/data/GoodsType.java
+++ src/main/java/com/littlebuddha/bobogou/modules/entity/data/GoodsType.java
@@ -3,6 +3,7 @@ package com.littlebuddha.bobogou.modules.entity.data;
 import com.littlebuddha.bobogou.common.utils.excel.ExcelField;
 import com.littlebuddha.bobogou.modules.base.entity.DataEntity;
 import com.littlebuddha.bobogou.modules.entity.system.Operator;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 商品分类实体类
@@ -12,10 +13,9 @@ public class GoodsType extends DataEntity<GoodsType> {
     private String name;//分类名称
     private String icon;//分类图标地址
     private Integer parentId;//父级分类ID，顶级=0
-    private Integer accountId;//最后操作人ID
+    private String accountId;//最后操作人ID
 
     private GoodsType goodsType;//图标外键
-    private Operator operator;//最后操作人外键
 
     @ExcelField(title = "分类名称", align = 2, sort = 1)
     public String getName() {
@@ -45,11 +45,14 @@ public class GoodsType extends DataEntity<GoodsType> {
     }
 
     @ExcelField(title = "最后操作人ID", align = 2, sort = 1)
-    public Integer getAccountId() {
+    public String getAccountId() {
+        if(this.updateBy != null && StringUtils.isNotBlank(updateBy.getId())){
+            this.accountId = updateBy.getId();
+        }
         return accountId;
     }
 
-    public void setAccountId(Integer accountId) {
+    public void setAccountId(String accountId) {
         this.accountId = accountId;
     }
 
@@ -59,13 +62,5 @@ public class GoodsType extends DataEntity<GoodsType> {
 
     public void setGoodsType(GoodsType goodsType) {
         this.goodsType = goodsType;
-    }
-
-    public Operator getOperator() {
-        return operator;
-    }
-
-    public void setOperator(Operator operator) {
-        this.operator = operator;
     }
 }
