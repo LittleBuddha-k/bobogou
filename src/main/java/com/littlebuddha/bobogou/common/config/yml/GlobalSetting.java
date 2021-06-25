8fc7058c5c20d59e64d5e399f6084302113017ba
LittleBuddha-k <2998986128@qq.com>
2021-06-25 12:50:21 +0800

    图片上传全局配置

---
+++ src/main/java/com/littlebuddha/bobogou/common/config/yml/GlobalSetting.java
@@ -0,0 +1,42 @@
+package com.littlebuddha.bobogou.common.config.yml;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: LittleBuddha-k
+ * \* Date: 2021-06-25
+ * \* Time: 上午 11:22
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ * 全局配置类
+ */
+@Component
+@ConfigurationProperties(prefix ="global")
+public class GlobalSetting {
+
+    @Value("${global.uploadPath}")
+    private String uploadImage;//图片全局配置保存上传路径
+
+    @Value("${global.domainName}")
+    private String domainName;//域名
+
+    public String getUploadImage() {
+        return uploadImage;
+    }
+
+    public void setUploadImage(String uploadImage) {
+        this.uploadImage = uploadImage;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+}
--- src/main/java/com/littlebuddha/bobogou/modules/controller/common/PictureUploadController.java
+++ src/main/java/com/littlebuddha/bobogou/modules/controller/common/PictureUploadController.java
@@ -1,10 +1,12 @@
 package com.littlebuddha.bobogou.modules.controller.common;
 
+import com.littlebuddha.bobogou.common.config.yml.GlobalSetting;
 import com.littlebuddha.bobogou.common.utils.FileUtils;
 import com.littlebuddha.bobogou.common.utils.MarkdownResult;
 import com.littlebuddha.bobogou.common.utils.Result;
 import com.littlebuddha.bobogou.common.utils.UserUtils;
 import com.littlebuddha.bobogou.modules.entity.system.Operator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +26,9 @@ import java.util.UUID;
 @Controller
 public class PictureUploadController {
 
+    @Autowired
+    private GlobalSetting globalSetting;
+
     @ResponseBody
     @PostMapping("/picture")
     public Result pictureUpload(HttpServletRequest request, HttpServletResponse response, MultipartFile file, Model model) throws IOException {
@@ -34,7 +39,7 @@ public class PictureUploadController {
         //String uploadPath = request.getParameter("uploadPath");
         String fileName = file.getOriginalFilename();  // 文件名
         String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
-        String filePath = "D://temp-rainy//"; // 上传后的路径
+        String filePath = globalSetting.getUploadImage(); // 上传后的路径
         fileName = UUID.randomUUID() + suffixName; // 新文件名
         File dest = new File(filePath + fileName);
         if (!dest.getParentFile().exists()) {
@@ -45,8 +50,9 @@ public class PictureUploadController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String filename = "/temp-rainy/" + fileName;
-        result.put("url","/bobogou"+filename);
+        String domainName = globalSetting.getDomainName();
+        String filename = filePath + "/" + fileName;
+        result.put("url",domainName +filename);
         return result;
     }
 
@@ -70,7 +76,7 @@ public class PictureUploadController {
         }
         String fileName = multipartFile.getOriginalFilename();  // 文件名
         String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
-        String filePath = "D://temp-rainy//"; // 上传后的路径
+        String filePath = globalSetting.getUploadImage(); // 上传后的路径
         fileName = UUID.randomUUID() + suffixName; // 新文件名
         File dest = new File(filePath + fileName);
         if (!dest.getParentFile().exists()) {
@@ -78,13 +84,13 @@ public class PictureUploadController {
         }
         try {
             multipartFile.transferTo(dest);
-            String filename = "/temp-rainy/" + fileName;
+            String filename = filePath + "/" + fileName;
             result.setSuccess(1);
-            result.setUrl("/bobogou"+filename);
+            String domainName = globalSetting.getDomainName();
+            result.setUrl(domainName + filename);
             result.setMessage("上传成功");
         } catch (IOException e) {
             e.printStackTrace();
-            String filename = "/temp-rainy/" + fileName;
             result.setSuccess(0);
             result.setUrl("");
             result.setMessage("上传失败:"+e.getMessage());
