cace9ae60cbc65bd7964b851e92770230a216a9f
LittleBuddha-k <2998986128@qq.com>
2021-07-12 09:24:49 +0800

    userMember新增字段添加完成

--- src/main/resources/templates/modules/other/userMemberForm.js
+++ src/main/resources/templates/modules/other/userMemberForm.js
@@ -7,6 +7,150 @@ layui.use(['upload', 'element', 'form', 'layedit', 'laydate'], function() {
         ,upload = layui.upload
         ,element = layui.element;
 
+    //多图片上传
+    upload.render({
+        elem: '#test1',
+        url: '/bobogou/file/picture?uploadPath='+"/data/banner",
+        multiple: true,
+        before: function(obj){
+            //预读本地文件示例，不支持ie8---------base64
+            obj.preview(function(index, file, result){
+                $('#demo1').append('<img src="'+ result +'" alt="'+ file.name +'" style="width: 92px;height: 92px;" class="layui-upload-img">')
+            });
+        },
+        done: function(res){
+            //上传完毕
+            var last_url = $("#businessCertificate").val();
+            var upload_image_url = "";
+            if(last_url){
+                upload_image_url = last_url+","+res.body.url;
+            }else {
+                upload_image_url = res.body.url;
+            }
+            $("#businessCertificate").val(upload_image_url);
+        }
+    });//多图片上传
+    upload.render({
+        elem: '#test2',
+        url: '/bobogou/file/picture?uploadPath='+"/data/banner",
+        multiple: true,
+        before: function(obj){
+            //预读本地文件示例，不支持ie8---------base64
+            obj.preview(function(index, file, result){
+                $('#demo2').append('<img src="'+ result +'" alt="'+ file.name +'" style="width: 92px;height: 92px;" class="layui-upload-img">')
+            });
+        },
+        done: function(res){
+            //上传完毕
+            var last_url = $("#foodBusinessLicense").val();
+            var upload_image_url = "";
+            if(last_url){
+                upload_image_url = last_url+","+res.body.url;
+            }else {
+                upload_image_url = res.body.url;
+            }
+            $("#foodBusinessLicense").val(upload_image_url);
+        }
+    });//多图片上传
+    upload.render({
+        elem: '#test3',
+        url: '/bobogou/file/picture?uploadPath='+"/data/banner",
+        multiple: true,
+        before: function(obj){
+            //预读本地文件示例，不支持ie8---------base64
+            obj.preview(function(index, file, result){
+                $('#demo3').append('<img src="'+ result +'" alt="'+ file.name +'" style="width: 92px;height: 92px;" class="layui-upload-img">')
+            });
+        },
+        done: function(res){
+            //上传完毕
+            var last_url = $("#authorityPurchase").val();
+            var upload_image_url = "";
+            if(last_url){
+                upload_image_url = last_url+","+res.body.url;
+            }else {
+                upload_image_url = res.body.url;
+            }
+            $("#authorityPurchase").val(upload_image_url);
+        }
+    });//多图片上传
+    upload.render({
+        elem: '#test4',
+        url: '/bobogou/file/picture?uploadPath='+"/data/banner",
+        multiple: true,
+        before: function(obj){
+            //预读本地文件示例，不支持ie8---------base64
+            obj.preview(function(index, file, result){
+                $('#demo4').append('<img src="'+ result +'" alt="'+ file.name +'" style="width: 92px;height: 92px;" class="layui-upload-img">')
+            });
+        },
+        done: function(res){
+            //上传完毕
+            var last_url = $("#mandatary").val();
+            var upload_image_url = "";
+            if(last_url){
+                upload_image_url = last_url+","+res.body.url;
+            }else {
+                upload_image_url = res.body.url;
+            }
+            $("#mandatary").val(upload_image_url);
+        }
+    });//多图片上传
+    upload.render({
+        elem: '#test5',
+        url: '/bobogou/file/picture?uploadPath='+"/data/banner",
+        multiple: true,
+        before: function(obj){
+            //预读本地文件示例，不支持ie8---------base64
+            obj.preview(function(index, file, result){
+                $('#demo5').append('<img src="'+ result +'" alt="'+ file.name +'" style="width: 92px;height: 92px;" class="layui-upload-img">')
+            });
+        },
+        done: function(res){
+            //上传完毕
+            var last_url = $("#groupPhoto").val();
+            var upload_image_url = "";
+            if(last_url){
+                upload_image_url = last_url+","+res.body.url;
+            }else {
+                upload_image_url = res.body.url;
+            }
+            $("#groupPhoto").val(upload_image_url);
+        }
+    });
+
+    /**
+     * 多图清除按钮点击事件
+     */
+    $("#btn_image_clear_test1").click(function () {
+        $('#demo1').html("");
+        $("#businessCertificate").val('');
+    });/**
+     * 多图清除按钮点击事件
+     */
+    $("#btn_image_clear_test2").click(function () {
+        $('#demo2').html("");
+        $("#foodBusinessLicense").val('');
+    });/**
+     * 多图清除按钮点击事件
+     */
+    $("#btn_image_clear_test3").click(function () {
+        $('#demo3').html("");
+        $("#authorityPurchase").val('');
+    });/**
+     * 多图清除按钮点击事件
+     */
+    $("#btn_image_clear_test4").click(function () {
+        $('#demo4').html("");
+        $("#mandatary").val('');
+    });/**
+     * 多图清除按钮点击事件
+     */
+    $("#btn_image_clear_test5").click(function () {
+        $('#demo5').html("");
+        $("#groupPhoto").val('');
+    });
+
     form.on('submit(pass)', function(data){
         let type = $("#type").val();
         //获取customerUser表单中的id信息
