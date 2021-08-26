0995287274126195d9fe59e57ba6e2436f60724a
LittleBuddha-k <2998986128@qq.com>
2021-08-26 08:54:24 +0800

    商品区域修改

--- src/main/resources/templates/modules/data/regionGoodsForm.js
+++ src/main/resources/templates/modules/data/regionGoodsForm.js
@@ -43,8 +43,7 @@ layui.use('form', function(){
         let provinceId = data.value;
         $("#city").empty();//清空城市选项
         $("#area").empty();//清空城市选项
-        $("#street").val("");//清空城市选项
-        $("#streetId").val("");//清空城市选项
+        $("#street").empty();//清空城市选项
         rc.post("/bobogou/data/city/all",{"province.id":provinceId},function(data){
             if(data.length>0) {
                 //对应的值传回，拼出html下拉框语句
@@ -61,8 +60,7 @@ layui.use('form', function(){
     form.on('select(city)', function(data){
         let cityId = data.value;
         $("#area").empty();//清空城市选项
-        $("#street").val("");//清空城市选项
-        $("#streetId").val("");//清空城市选项
+        $("#street").empty();//清空城市选项
         rc.post("/bobogou/data/area/all",{"city.id":cityId},function(data){
             if(data.length>0) {
                 //对应的值传回，拼出html下拉框语句
@@ -78,8 +76,7 @@ layui.use('form', function(){
     //下拉框选中后的时间
     form.on('select(area)', function(data){
         let streetId = data.value;
-        $("#street").val("");//清空城市选项
-        $("#streetId").val("");//清空城市选项
+        $("#street").empty();//清空城市选项
         rc.post("/bobogou/data/street/all",{"area.id":streetId},function(data){
             if(data.length>0) {
                 //对应的值传回，拼出html下拉框语句
@@ -112,6 +109,11 @@ function save(parentIndex) {
     if(!isValidate){
         return false;
     }else {
+        //提交前设置disabled失效
+        $("#province").removeAttr("disabled");
+        $("#city").removeAttr("disabled");
+        $("#area").removeAttr("disabled");
+        $("#street").removeAttr("disabled");
         $.ajax({
             url: "/bobogou/data/regionGoods/save",    //请求的url地址
             dataType: "json",   //返回格式为json
