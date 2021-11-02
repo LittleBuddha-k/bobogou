62a8ee9a9fa30c0aee4bb7f5edaa9ea38ca50721
LittleBuddha-k <2998986128@qq.com>
2021-11-02 17:27:12 +0800

    商品区域多选修改完成

--- src/main/resources/templates/modules/data/regionGoodsForm.js
+++ src/main/resources/templates/modules/data/regionGoodsForm.js
@@ -56,61 +56,6 @@ layui.use('form', function(){
             form.render();
         }
     });
-
-/*    //下拉框选中后的时间
-    form.on('select(province)', function(data){
-        let provinceId = data.value;
-        $("#city").empty();//清空城市选项
-        $("#area").empty();//清空城市选项
-        $("#street").empty();//清空城市选项
-        rc.post("/bobogou/data/city/all",{"province.id":provinceId},function(data){
-            if(data.length>0) {
-                //对应的值传回，拼出html下拉框语句
-                var tmp='<option value="0">请选择</option>';
-                $("#area").html(tmp);
-                $("#street").html(tmp);
-                for(let i=0;i<data.length;i++) {
-                    tmp += "<option value='" + data[i].id + "'>" + data[i].name + "</option>";
-                }
-                $("#city").html(tmp);
-                form.render();
-            }
-        })
-    });
-    //下拉框选中后的时间
-    form.on('select(city)', function(data){
-        let cityId = data.value;
-        $("#area").empty();//清空城市选项
-        $("#street").empty();//清空城市选项
-        rc.post("/bobogou/data/area/all",{"city.id":cityId},function(data){
-            if(data.length>0) {
-                //对应的值传回，拼出html下拉框语句
-                var tmp='<option value="0">请选择</option>';
-                $("#street").html(tmp);
-                for(let i=0;i<data.length;i++) {
-                    tmp += "<option value='" + data[i].id + "'>" + data[i].name + "</option>";
-                }
-                $("#area").html(tmp);
-                form.render();
-            }
-        })
-    });
-    //下拉框选中后的时间
-    form.on('select(area)', function(data){
-        let streetId = data.value;
-        $("#street").empty();//清空城市选项
-        rc.post("/bobogou/data/street/all",{"area.id":streetId},function(data){
-            if(data.length>0) {
-                //对应的值传回，拼出html下拉框语句
-                var tmp='<option value="0">请选择</option>';
-                for(let i=0;i<data.length;i++) {
-                    tmp += "<option value='" + data[i].id + "'>" + data[i].name + "</option>";
-                }
-                $("#street").html(tmp);
-                form.render();
-            }
-        })
-    });*/
 });
 
 $(document).ready(function () {
@@ -132,10 +77,10 @@ function save(parentIndex) {
         return false;
     }else {
         //提交前设置disabled失效
-        $("#province").removeAttr("disabled");
-        $("#city").removeAttr("disabled");
-        $("#area").removeAttr("disabled");
-        $("#street").removeAttr("disabled");
+        $("#provinceId").removeAttr("disabled");
+        $("#cityId").removeAttr("disabled");
+        $("#districtId").removeAttr("disabled");
+        $("#streetId").removeAttr("disabled");
         $.ajax({
             url: "/bobogou/data/regionGoods/save",    //请求的url地址
             dataType: "json",   //返回格式为json
