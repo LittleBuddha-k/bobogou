be01c44dc2ae959f075a17b140b35033a1be5b98
LittleBuddha-k <2998986128@qq.com>
2021-11-08 10:29:33 +0800

    新增订单厂家列表数据权限

--- src/main/java/com/littlebuddha/bobogou/modules/service/data/OrderFactoryService.java
+++ src/main/java/com/littlebuddha/bobogou/modules/service/data/OrderFactoryService.java
@@ -1,18 +1,27 @@
 package com.littlebuddha.bobogou.modules.service.data;
 
 import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.littlebuddha.bobogou.common.utils.ListUtils;
+import com.littlebuddha.bobogou.common.utils.PageUtil;
+import com.littlebuddha.bobogou.common.utils.UserUtils;
 import com.littlebuddha.bobogou.modules.base.service.CrudService;
 import com.littlebuddha.bobogou.modules.entity.data.Order;
 import com.littlebuddha.bobogou.modules.entity.data.OrderFactory;
+import com.littlebuddha.bobogou.modules.entity.system.Operator;
+import com.littlebuddha.bobogou.modules.entity.system.OperatorRegion;
 import com.littlebuddha.bobogou.modules.mapper.data.OrderFactoryMapper;
 import com.littlebuddha.bobogou.modules.mapper.data.OrderMapper;
+import com.littlebuddha.bobogou.modules.mapper.system.OperatorRegionMapper;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,6 +30,12 @@ public class OrderFactoryService extends CrudService<OrderFactory, OrderFactoryM
 
     @Resource
     private OrderMapper orderMapper;
+
+    @Resource
+    private OperatorRegionMapper operatorRegionMapper;
+
+    @Resource
+    private OrderFactoryMapper orderFactoryMapper;
 
     @Override
     public OrderFactory get(OrderFactory entity) {
@@ -52,14 +67,56 @@ public class OrderFactoryService extends CrudService<OrderFactory, OrderFactoryM
             String orderNumber = StringUtils.deleteWhitespace(entity.getOrderNumber());
             entity.setOrderNumber(orderNumber);
         }
-        PageInfo<OrderFactory> page1 = super.findPage(page, entity);
-        List<OrderFactory> list = page1.getList();
-        for (OrderFactory orderFactory : list) {
-            if (orderFactory != null && orderFactory.getFreight() != null){
-                orderFactory.setFreight(orderFactory.getFreight() / 100);
+        //封装的结果集
+        List<OrderFactory> result = new ArrayList<>();
+        //获取当前用户区域
+        Operator currentUser = UserUtils.getCurrentUser();
+        entity.setCurrentUser(currentUser);
+        OperatorRegion operatorRegion = new OperatorRegion();
+        operatorRegion.setOperatorId(currentUser.getId());
+        //查询当前用户的所有区域list
+        List<OperatorRegion> operatorRegionList = operatorRegionMapper.getOperatorRegionByCurrentUser(operatorRegion);
+        //查询封装结果
+        PageInfo<OrderFactory> pageInfo = null;
+        //根据当前用户区域查询对应发货单位数据
+        if (entity.getPageNo() != null && entity.getPageSize() != null) {
+            if (currentUser != null && "1".equals(currentUser.getId())){//如果是超级管理员查询全部数据
+                result = orderFactoryMapper.findList(entity);
+            }else {
+                for (OperatorRegion region : operatorRegionList) {//按照区域来进行查询
+                    if (region != null) {
+                        entity.setProvinceId(region.getProvinceId());
+                        entity.setCityId(region.getCityId());
+                        entity.setAreaId(region.getDistrictId());
+                        entity.setStreetId(region.getStreetId());
+                        List<OrderFactory> factoryList = orderFactoryMapper.findList(entity);
+                        if (factoryList != null) {
+                            result.addAll(factoryList);
+                        }
+                    }
+                }
+            }
+            //去重
+            //result = ListUtils.removeDuplicateFactory(result);
+            //分页数据
+            List<OrderFactory> list = PageUtil.startPage(result, entity.getPageNo(), entity.getPageSize());
+            if (list != null && !list.isEmpty()) {
+                for (OrderFactory orderFactory : list) {
+                    if (orderFactory != null && orderFactory.getFreight() != null) {
+                        orderFactory.setFreight(orderFactory.getFreight() / 100);
+                    }
+                }
+            }
+            //组装结果
+            if (result != null && !result.isEmpty()) {
+                pageInfo = new PageInfo<>();
+                pageInfo.setList(list);
+                pageInfo.setTotal(result.size());
+            } else {
+                pageInfo = new PageInfo<>();
             }
         }
-        return page1;
+        return pageInfo;
     }
 
     @Override
