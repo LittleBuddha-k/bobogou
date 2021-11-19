72b9439e4a5dfa851ab0fd26ed491fbcdd15e898
LittleBuddha-k <2998986128@qq.com>
2021-11-19 21:51:15 +0800

    用户列表调整列宽

--- src/main/resources/templates/modules/other/customerUser.js
+++ src/main/resources/templates/modules/other/customerUser.js
@@ -35,9 +35,7 @@ layui.use(['form', 'table'], function () {
                 {
                     title: '手机号',
                     field: 'phone',
-                    sort: true,
-                    sortName: 'phone',
-                    width: '180',
+                    minWidth: 120,
                     templet:function(data){
                         var valueArray = data.phone;
                         return valueArray;
@@ -46,16 +44,11 @@ layui.use(['form', 'table'], function () {
                 {
                     title: '昵称',
                     field: 'nickname',
-                    sort: true,
-                    sortName: 'nickname',
-                    width: '180'
+                    minWidth: 120,
                 },
                 {
                     title: '性别',
                     field: 'sex',
-                    sort: true,
-                    sortName: 'sex',
-                    width: '120',
                     templet:function(data){
                         var sex = data.sex;
                         let dictName = rc.getDictName("user_member_sex",sex);
@@ -68,9 +61,7 @@ layui.use(['form', 'table'], function () {
                 {
                     title: '会员等级',
                     field: 'member',
-                    sort: true,
-                    sortName: 'member',
-                    width: '180',
+                    minWidth: 100,
                     templet:function(data){
                         var member = data.member;
                         let dictName = rc.getDictName("user_member_level",member);
@@ -83,28 +74,20 @@ layui.use(['form', 'table'], function () {
                 {
                     title: '积分',
                     field: 'integral',
-                    sort: true,
-                    width: '120',
-                    sortName: 'integral'
                 },
                 {
                     title: '健康豆',
                     field: 'healthBeans',
-                    sort: true,
-                    width: '100',
-                    sortName: 'healthBeans'
                 },
                 {
                     title: '签到时间',
                     field: 'signInTime',
-                    sort: true,
-                    sortName: 'signInTime',
-                    width: '170',
+                    minWidth: 170,
                 },
                 {
                     title: 'VIP过期日期',
                     field: 'vipExpire',
-                    width: '170',
+                    minWidth: 170,
                     templet:function(data){
                         var vipExpire = data.vipExpire;
                         if (vipExpire != null){
@@ -117,9 +100,7 @@ layui.use(['form', 'table'], function () {
                 {
                     title: '消息接收状态',
                     field: 'messageStatus',
-                    sort: true,
-                    sortName: 'messageStatus',
-                    width: '180',
+                    minWidth: 100,
                     templet:function(data){
                         var messageStatus = data.messageStatus;
                         let dictName = rc.getDictName("user_member_message_status",messageStatus);
@@ -132,9 +113,7 @@ layui.use(['form', 'table'], function () {
                 {
                     title: '是否同意用户协议',
                     field: 'userAgreement',
-                    sort: true,
-                    sortName: 'userAgreement',
-                    width: '200',
+                    minWidth: 150,
                     templet:function(data){
                         var userAgreement = data.userAgreement;
                         let dictName = rc.getDictName("user_member_user_agreement",userAgreement);
@@ -147,9 +126,7 @@ layui.use(['form', 'table'], function () {
                 {
                     title: '会员申请状态',
                     field: 'applyStatus',
-                    sort: true,
-                    sortName: 'applyStatus',
-                    width: '180',
+                    minWidth: 150,
                     templet:function(data){
                         var applyStatus = data.applyStatus;
                         let dictName = rc.getDictName("user_member_apply_status",applyStatus);
@@ -162,8 +139,7 @@ layui.use(['form', 'table'], function () {
                 {
                     title: '操作',
                     toolbar: '#operation',
-                    align: "center",
-                    width: '200'
+                    align: "center"
                 }
             ]
         ],
